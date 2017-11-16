faeb0ee7cfd00714e6323d26e834486fbc517473
mmanahi <m.menahi@gmail.com>
2017-11-16 20:23:23 +0200

    add app styles, scripts, libraries cdns and images

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
 class StaticPagesController < ApplicationController
-  def index
+  def product
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/index', to: 'static_pages#index'
-
+  get '/about', to: 'static_pages#about'
+  get '/product', to: 'static_pages#product'
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
