f92c51aa15ee7b21ea66f8ed1ae00744b2fb97dd
mmanahi <m.menahi@gmail.com>
2017-11-17 22:10:46 +0200

    optimize app and improve front-end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,54 +12,63 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require jquery3
+//= require jquery_ujs
 //= require_tree .
-(function($, document, window){
 
-	$(document).ready(function(){
-
-		// Cloning main navigation for mobile menu
-		$(".mobile-navigation").append($(".main-navigation .menu").clone());
-
-		// Mobile menu toggle
-		$(".toggle-menu").click(function(){
-			$(".mobile-navigation").slideToggle();
-		});
-
-		$(".home-slider").flexslider({
-			controlNav: true,
-			directionNav: false
-		});
-
-		$(".login-button").on("click",function(){
-			$(".overlay").fadeIn();
-			$(".auth-popup").toggleClass("active");
-		});
-
-		$(".close, .overlay").on("click",function(){
-			$(".overlay").fadeOut();
-			$(".popup").toggleClass("active");
-		});
-
-		initLightbox({
-	    	selector : '.product-images a',
-	    	overlay: true,
-	    	closeButton: true,
-	    	arrow: true
-	    });
-
-
-		$(document).keyup(function(e) {
-			if( $(".popup").hasClass("active")){
-		  		if (e.keyCode === 27) {
-		  			$(".overlay").fadeOut();
-					$(".popup").toggleClass("active");
-		  		}
-			}
-		});
+$(document).ready(function() {
+  $("#list").mouseenter(function() {
+     $(this).css("color", "#45b77d");
+  });
+	$("#list").mouseleave(function() {
+		 $(this).css("color", "#fff");
 	});
+});
 
-	$(window).load(function(){
+(function($, document, window) {
 
-	});
+  $(document).ready(function() {
+
+    // Cloning main navigation for mobile menu
+    $(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+    // Mobile menu toggle
+    $(".toggle-menu").click(function() {
+      $(".mobile-navigation").slideToggle();
+    });
+
+    $(".login-button").on("click", function() {
+      $(".overlay").fadeIn();
+      $(".auth-popup").toggleClass("active");
+    });
+
+    $(".close, .overlay").on("click", function() {
+      $(".overlay").fadeOut();
+      $(".popup").toggleClass("active");
+    });
+
+    initLightbox({
+      selector: '.product-images a',
+      overlay: true,
+      closeButton: true,
+      arrow: true
+    });
+
+
+    $(document).keyup(function(e) {
+      if ($(".popup").hasClass("active")) {
+        if (e.keyCode === 27) {
+          $(".overlay").fadeOut();
+          $(".popup").toggleClass("active");
+        }
+      }
+    });
+
+  });
+
+  $(window).load(function() {
+
+  });
+
 
 })(jQuery, document, window);
