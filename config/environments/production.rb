5133f94da7339664ebed53d3852f8094c9103599
mmanahi <m.menahi@gmail.com>
2017-11-18 14:44:43 +0200

    fix js assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.precompile = true
+  config.assets.compile = true
   config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
