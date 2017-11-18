7cbd18f0772810349b208c53eed053a4d0b4da24
mmanahi <m.menahi@gmail.com>
2017-11-18 10:47:52 +0200

    fix  precompiling all js css assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  Rails.application.config.assets.precompile += %w( *.js *.css )
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
