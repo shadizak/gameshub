f257aec21aa8824c548cb4e11f97c40601e2c33c
mmanahi <m.menahi@gmail.com>
2017-11-19 16:11:15 +0200

    add products category, price using db migrations, and update pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,13 +62,13 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:name, :description, :image_url)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :category, :description, :image_url, :price)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118194932) do
+ActiveRecord::Schema.define(version: 20171119134934) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20171118194932) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.decimal "price"
   end
 
 end
