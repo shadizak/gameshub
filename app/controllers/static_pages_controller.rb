a5dcf687c8e6e66da8d2263a035e272ca6beac9e
mmanahi <m.menahi@gmail.com>
2017-11-26 11:11:57 +0200

    fix dynamic indicators to ensure sepration of concerns in MVC pattern

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
     @products = Product.limit(5)
+    products_with_image_url = @products.where.not('products.image_url' => nil).count
+    @indicators =  (0...products_with_image_url)
   end
 
   def featured
