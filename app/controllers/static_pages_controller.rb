8cf76f2cad676cff6b728128bc671bf7516b40bc
mmanahi <m.menahi@gmail.com>
2017-11-28 10:12:18 +0200

    fix ui slider

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,15 @@
 class StaticPagesController < ApplicationController
   def index
     @products = Product.limit(5)
-    products_with_image_url = @products.where.not('products.image_url' => nil).count
-    @indicators =  (0...products_with_image_url)
+    dynamic_indicator
   end
 
   def featured
     @featured_product = Product.first
+  end
+
+  def dynamic_indicator
+    products_with_image_url = @products.where.not('products.image_url' => nil).count
+    @indicators =  (0...products_with_image_url)
   end
 end
