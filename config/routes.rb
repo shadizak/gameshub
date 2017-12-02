9d8ad108abaede20776d48b9943485f3c8c70485
mmanahi <m.menahi@gmail.com>
2017-12-02 13:44:49 +0200

    update nav-bar links, redirecting products#create action to navigate to products#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  root 'products#index'
 
   get '/index', to: 'static_pages#index'
   get '/about', to: 'static_pages#about'
   get '/featured', to: 'static_pages#featured'
   resources :products
+  # get '/products/:id', to: 'products#show'
   resources :orders, only: [:index, :show, :create, :destroy]
+  # resources :orders, except: [:new, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
