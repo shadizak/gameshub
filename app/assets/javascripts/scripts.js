1bf337aa6bddce97d9fe057a013d0c4c5c19f786
mmanahi <m.menahi@gmail.com>
2017-12-05 13:37:18 +0200

    use js search product script inside products#index view

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,4 +1,9 @@
 // script for scrolling user when hitting seach in (products#index)
-$(document).ready(function(){
-     $('body,html').animate({scrollTop: 600}, 800);
+$(document).ready(function() {
+  // $('body,html').animate({
+  //   scrollTop: 600
+  // }, 800);
+  $('html, body').animate({
+       scrollTop: $("#search_result").offset().top + offset
+     }, 2000);
 });
