03afa2b9900cacf49fa2de799ca7dff103ecdb80
mmanahi <m.menahi@gmail.com>
2017-12-06 15:09:33 +0200

    update contact form recieved email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,7 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
     @message = message
-    mail(from: email,
-      to: 'cfgameshub@gmail.com',
+    mail( to: 'cfgameshub@gmail.com',
     subject: "A new contact form message from #{name}")
   end
 end
