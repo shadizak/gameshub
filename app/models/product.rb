376ad8b0ad914325462e1577335f877f1a4775a8
mmanahi <m.menahi@gmail.com>
2017-12-18 13:44:41 +0200

    add comment, ratings to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   # define scope for fetching products that have images
   scope :products_with_images, -> {where.not(image_url: nil)}
@@ -8,10 +9,21 @@ class Product < ApplicationRecord
     # sqllite and postgres are excute search query differently, sqllite uses 'LIKE', postgres uses 'ilike'
     query_env = Rails.env.production? ? 'ilike' : 'LIKE'
     Product.where("name #{query_env} ?", "%#{search_term}%")
-
   end
 
   def self.dynamic_indicator
     products_with_images.count
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def avg_rating_comment
+    comments.average('rating')
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
   end
 end
