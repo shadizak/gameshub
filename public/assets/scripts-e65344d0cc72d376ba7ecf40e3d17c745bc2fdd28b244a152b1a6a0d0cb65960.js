adb15874df870dd1570518d8df427ede27b32ed4
mmanahi <m.menahi@gmail.com>
2017-12-20 13:47:49 +0200

    fix font paths for raty.js lib

---
+++ public/assets/scripts-e65344d0cc72d376ba7ecf40e3d17c745bc2fdd28b244a152b1a6a0d0cb65960.js
@@ -0,0 +1,22 @@
+$(document).on('turbolinks:load', function() {
+
+    $(".alert").delay(1000).fadeOut(5000);
+
+    $('.rating').raty({
+      starHalf: '/assets/star-half-db15fb9b3561d5c741d8aea9ef4f0957bd9bc51aa1caa6d7a5c316e083c1abd5.png',
+      starOff: '/assets/star-off-6aaeebdaab93d594c005d366ce0d94fba02e7a07fd03557dbee8482f04a91c22.png',
+      starOn: '/assets/star-on-fd26bf0ea0990cfd808f7540f958eed324b86fc609bf56ec2b3a5612cdfde5f5.png',
+      scoreName: 'comment[rating]'
+      });
+
+      $('.rated').raty({
+      starHalf: '/assets/star-half-db15fb9b3561d5c741d8aea9ef4f0957bd9bc51aa1caa6d7a5c316e083c1abd5.png',
+      starOff: '/assets/star-off-6aaeebdaab93d594c005d366ce0d94fba02e7a07fd03557dbee8482f04a91c22.png',
+      starOn: '/assets/star-on-fd26bf0ea0990cfd808f7540f958eed324b86fc609bf56ec2b3a5612cdfde5f5.png',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+
+  });
