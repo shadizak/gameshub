0d60e9c38bf87d7902628469ad2a4e2a1e733199
mmanahi <m.menahi@gmail.com>
2017-12-24 20:26:12 +0200

    add index and show logic to orders controller, add orders count in products index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, alert: exception.message
   end
+  # perform before_action only if the controller is devise_controller
   before_action :config_premitted_params, if: :devise_controller?
   protected
 
+  # customize devise registration, add first_name and last_name to sign up
   def config_premitted_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,16 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
+    @orders = Order.includes(:product, :user).all
   end
   def new
   end
   def show
+    @order = Order.find(params[:id])
   end
   def create
   end
   def destroy
   end
+
 end
