5e9e0af7eeff0acc007b70bd43583761d2db34f9
mmanahi <m.menahi@gmail.com>
2018-01-20 10:47:42 +0200

    UI optimizations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
       else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+        format.html { redirect_to @product, alert: 'Review was not saved successfully, review must fulfill comment and rating!' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
